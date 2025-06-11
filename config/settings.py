4ebf957f116cae65de7db0af23120e25900091bf
Charles Adams 19 <adamscra19@gmail.com>
2025-06-11 14:30:16 +0100

    deploy

--- config/settings.py
+++ config/settings.py
@@ -30,10 +30,30 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1'] # local frontend urls without protocols
 
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:5173", # Local frontend url
+    ]
+
+    CSRF_TRUSTED_ORIGINS = [
+        "http://127.0.0.1:8000", # Local backend url
+        "http://localhost:8000" # localhost for good measure
+    ]
+
+else:
+    ALLOWED_HOSTS = [env('DEPLOYED_BACKEND_URL').replace('https://', '')] # removes protocol (https://)
+
+    CORS_ALLOWED_ORIGINS = [
+        env('DEPLOYED_FRONTEND_URL'), # Deployed frontend url only
+    ]
+
+    CSRF_TRUSTED_ORIGINS = [
+        env('DEPLOYED_BACKEND_URL'), # Deployed backend url only
+    ]
 
 # Application definition
 
@@ -54,8 +74,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # New line, add this.
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,7 +162,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# This is the URL where the assets can be publicly accessed
+STATIC_URL = '/static/'
+
+# Tell Django the absolute path to store those assets - call the folder `staticfiles`
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# This production code might break development mode, so we check whether we're in DEBUG mode before setting the STATICFILES_STORAGE
+if not DEBUG:
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
--- itineraries/serializers.py
+++ itineraries/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from .models import Itinerary
 
 class ItinerarySerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Itinerary
         fields = '__all__'
         read_only_fields = ['user', 'created_at']
+
+    def get_user(self, obj):
+        return {
+            'id': obj.user.id,
+            'username': obj.user.username
+        }
